650230be521d6e9314724429cb18ecfaf182685f
jfxo0 <janisha.ly.wong@gmail.com>
2026-05-25 17:21:08 +0200

    added nav component & about me page

---
+++ components/nav.js
@@ -0,0 +1,17 @@
+const nav = document.getElementById("nav");
+
+nav.innerHTML = `
+  <nav class="navbar">
+ <nav>
+    <ul>
+        <li><a href="../index.html">Home</a></li>
+        <li><a href="../html/about.html">about</a></li>
+        <li><a href="../html/rules.html">Rules</a></li>
+        <!--        <li>-->
+        <!--          <p>under construction..</p>-->
+        <!--        </li>-->
+    </ul>
+</nav>
+  </nav>
+`;
+
